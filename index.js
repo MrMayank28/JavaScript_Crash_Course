21aa1f7a087bf947203ade7f0af2885c24f11baa
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-08 21:49:30 +0000

    variables and dataTypes

---
+++ index.js
@@ -0,0 +1,10 @@
+// Ways To Print In JavaScript
+console.log("Hello World...!!!");
+// alert("Mayank Tutorials")    // In General We Don't User Alert
+document.write("This is document write")
+
+// JavaScript Console API
+console.log("Hello World...!!!", 6 + 2, "Another Log");
+console.warn("This Is A Warning...")
+console.error("This Is An Error")
+// console.clear()
---
+++ variables_and_dataType.js
@@ -0,0 +1,49 @@
+// What are variables? : Container To Store Data Value
+
+let num1 = 36
+let num2 = 64
+console.log(num1 + num2);
+
+// Data Types In JavaScript
+// 1. Numbers
+let num3 = 45
+let num4 = 50.50
+
+// 2. String
+let str1 = "This is a string"
+let str2 = "This is also a string"
+
+// 3. Objects
+let marks = {
+    Mayank: 99,
+    Prachi: 98,
+    Akash: 95
+}
+console.log(marks);
+
+// 4. Booleans
+let a = true
+let b = false
+console.log(a, b);
+
+// 5. Undefined
+let und = undefined
+let undef;
+console.log(und, undef);
+
+// 6. null
+let n = null
+console.log(n);
+
+/*
+At a very high level, There are two types of data Types in JavaSript
+1. Primitive Data Types: undefinedd, null, number, string, boolean, symbol
+2. Reference (Non-Primitive) Data Types: objects, Arrays, Functions
+*/
+
+let arr = [1,2,3,4,5]
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
