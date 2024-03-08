aeddd53d67e14d61f8dbef0f5074668d183fa613
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-08 22:16:50 +0000

    operators concept

---
+++ operators.js
@@ -0,0 +1,52 @@
+// Arithmetic Operators
+
+let num5 = 15
+let num6 = 25
+
+// console.log("The Value of a + b ", num5 + num6);
+// console.log("The Value of a - b ", num5 - num6);
+// console.log("The Value of a * b ", num5 * num6);
+// console.log("The Value of a / b ", num5 / num6);
+// console.log("The Value of a % b ", num5 % num6);
+// console.log("The Value of a ** b ", num5 ** num6);
+
+// Assignment Operators
+let num7 = num5
+num7 += 2;
+num7 -= 2;  // num7 = num7 - 2;
+num7 *= 2;
+num7 /= 2;
+// console.log(num3);
+
+// Comparision Operators
+let x = 45;
+let y = 65;
+
+// console.log(x > y);
+// console.log(x >= y);
+// console.log(x < y);
+// console.log(x <= y);
+
+// // Compare only Values
+// console.log(x == y);
+
+// // Strict Check Values And DataTypes Both
+// console.log(x === y);
+
+// Logical Operators
+
+// Logical AND
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && false);
+// console.log(false && true);
+
+// Logical OR
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
+// console.log(false || true);
+
+// Logical NOT
+// console.log(!true);
+// console.log(!true);
--- variables_and_dataType.js
+++ variables_and_dataType.js
@@ -42,8 +42,8 @@ At a very high level, There are two types of data Types in JavaSript
 */
 
 let arr = [1,2,3,4,5]
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
