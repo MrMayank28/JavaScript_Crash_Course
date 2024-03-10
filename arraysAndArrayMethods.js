85a38adba442eed34775e030f078b45b6570232a
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-10 08:43:15 +0000

    Array and Array Concepts

---
+++ arraysAndArrayMethods.js
@@ -0,0 +1,32 @@
+let myArr = ['Fan', 'Camera', 34, null, undefined, true];
+
+// console.log(myArr.length);
+
+
+// myArr.pop();    // Removed Last Element From The Array
+
+
+// Add One Or Series Of Elements At The Last Of The Array
+// myArr.push("Mayank");    
+// myArr.push("Prachi", false)
+
+
+// myArr.shift();   // Remove First Element From The Array
+
+
+// myArr.unshift(true);    // Add One Or Series Of Element At The First Place Of The Array
+
+
+// It Returns the Length Of The New Array
+// const newLen = myArr.unshift(true)
+// console.log(newLen);
+
+
+//Convert The Elements Into String
+// console.log(myArr.toString());
+// Output: Fan,Camera,34,,,true (It Skips The null and undefined values from the array)
+
+
+// First It Convert The Element Into String And Sort The Array In Ascending Order
+// console.log(myArr.sort());
+
---
+++ loops.js
@@ -0,0 +1,61 @@
+let arr = [1, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9]
+// console.log(arr);
+
+// For Loop
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log(i, arr[i]);
+// }
+
+
+// ForEach Loop
+
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+
+const a = 0;
+// a = a + 1;
+// a++;
+
+
+// While Loop
+
+// let j = 0;
+// while (j < arr.length) {
+//     console.log(arr[j]);
+//     j++;
+// }
+
+
+// Do While Loop
+
+// let j = 0;
+// do {
+//     console.log(arr[j]);
+//     j++
+// }
+// while (j < arr.length);
+
+
+// Break Statement: Stop The Loop And Come Out Of The Loop
+// And
+// Continue Statement: Skip One Step And Continue Further Loop
+
+// Break
+
+// for(let i = 0; i < arr.length; i++){
+//     if (i == 2){
+//         break;
+//     }
+//     console.log(i, arr[i]);
+// }
+
+// Continue
+
+// for(let i = 0; i < arr.length; i++){
+//     if (i == 2){
+//         continue;
+//     }
+//     console.log(i, arr[i]);
+// }
