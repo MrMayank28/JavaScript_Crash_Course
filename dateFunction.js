5db80c034105f13b7ea4052ce67954b2ddeff674
Mayank Borasi <124684749+MrMayank28@users.noreply.github.com>
2024-03-10 15:14:28 +0000

    String And Date And Their Methods

---
+++ dateFunction.js
@@ -0,0 +1,23 @@
+let myDate = new Date();
+
+console.log(myDate);
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getSeconds());
+// console.log(myDate.getMilliseconds());
+
+// console.log(myDate.getTime());   // Complete Time In Seconds
+
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.getUTCDate());
+// console.log(myDate.getUTCDay());
+// console.log(myDate.getUTCFullYear());
+// console.log(myDate.getUTCHours());
+// console.log(myDate.getUTCMilliseconds());
+// console.log(myDate.getUTCMinutes());
+// console.log(myDate.getUTCMonth());
+// console.log(myDate.getUTCSeconds());
---
+++ stringAndStringMethods.js
@@ -0,0 +1,12 @@
+let myString = "Mayank is a good boy and also has a good nature with a good sense of humour"
+
+// console.log(myString.indexOf("good"));
+// console.log(myString.lastIndexOf("good"));
+// console.log(myString.slice(0,4));
+// console.log(myString.slice(-10,-8)); // Slice The String From The Last
+// console.log(myString.split(" ").reverse().join());   // Reverse The String
+
+// let rep = myString.replace("Mayank", "Prachi")
+// rep = rep.replace("boy", "girl")
+// console.log(rep, "\n", myString);
+
